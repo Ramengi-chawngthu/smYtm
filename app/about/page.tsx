90c3ef88122717d8ff29351c6a0e9fb0c6a32fe8
ramengi-chawngthu <sccc@email.com>
2023-05-15 14:18:42 +0530

    Added map emoji about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,8 @@ function About() {
     <h1 className="text-center text-2xl md:text-4xl mt-12">
         He SM & YTM Movement hi ram danga rawngbawla kal leh la kal tur ho chanchin ziahna a ni.
     </h1>
-    <h1 className={"text-center text-5xl md:text-8xl font-bold mt-36 " + styles.commission}>Thupek Ropui tak chu</h1>
-    <p className="mt-16 text-2xl md:text-4xl text-center">Tin, he Ram Chanchin Ṭha hi hnam zawng zawng hriattîrna tûrin khawvêl zawng zawngah hrilin a awm ang a; chumi zawhah chuan tâwpna chu a lo thleng ang.</p>
+    <h1 className={"text-center text-5xl md:text-8xl font-bold mt-36 " + styles.commission}>Thupek Ropui tak chu 🗺</h1>
+    <p className="mt-16 text-2xl md:text-4xl text-center">Tin, he Ram Chanchin Ṭha hi hnam zawng zawng hriattîrna tûrin khawvêl zawng zawngah hrilin a awm ang a; chumi zawhah chuan tâwpna chu a lo thleng ang</p>
     <p className="text-right text-xl md:text-2xl mt-4 font-bold">Matthaia 24:14</p>
     </>
   )
