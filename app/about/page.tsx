266abd86f23aa3e536219e553bbd434956f7b4cb
ramengi-chawngthu <sccc@email.com>
2023-05-15 14:14:37 +0530

    Fixed spacing in posts page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,12 +3,12 @@ import styles from './styles.module.css'
 function About() {
   return (
     <>
-    <h1 className="text-center text-2xl">
+    <h1 className="text-center text-2xl md:text-4xl mt-12">
         He SM & YTM Movement hi ram danga rawngbawla kal leh la kal tur ho chanchin ziahna a ni.
     </h1>
-    <h1 className={"text-center text-5xl font-bold mt-20 " + styles.commission}>Thupek Ropui tak chu</h1>
-    <p className="mt-8 text-2xl text-center">Tin, he Ram Chanchin Ṭha hi hnam zawng zawng hriattîrna tûrin khawvêl zawng zawngah hrilin a awm ang a; chumi zawhah chuan tâwpna chu a lo thleng ang.</p>
-    <p className="text-right text-xl mt-4 font-bold">Matthaia 24:14</p>
+    <h1 className={"text-center text-5xl md:text-8xl font-bold mt-36 " + styles.commission}>Thupek Ropui tak chu</h1>
+    <p className="mt-16 text-2xl md:text-4xl text-center">Tin, he Ram Chanchin Ṭha hi hnam zawng zawng hriattîrna tûrin khawvêl zawng zawngah hrilin a awm ang a; chumi zawhah chuan tâwpna chu a lo thleng ang.</p>
+    <p className="text-right text-xl md:text-2xl mt-4 font-bold">Matthaia 24:14</p>
     </>
   )
 }
