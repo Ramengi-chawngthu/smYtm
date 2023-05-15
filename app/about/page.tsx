19f386d940cfb8afea041abf6cd8a49869908cc9
ramengi-chawngthu <sccc@email.com>
2023-05-15 14:22:46 +0530

    Change title in About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <>
     <h1 className="text-center text-2xl md:text-4xl mt-12">
-        He SM & YTM Movement hi ram danga rawngbawla kal leh la kal tur ho chanchin ziahna a ni.
+        He SM & YTM Movement hi 10/40 window huam chhung rama rawngbawla kal leh la kal tur ho chanchin ziahna a ni.
     </h1>
     <h1 className={"text-center text-5xl md:text-8xl font-bold mt-36 " + styles.commission}>Thupek Ropui tak chu 🗺</h1>
     <p className="mt-16 text-2xl md:text-4xl text-center">Tin, he Ram Chanchin Ṭha hi hnam zawng zawng hriattîrna tûrin khawvêl zawng zawngah hrilin a awm ang a; chumi zawhah chuan tâwpna chu a lo thleng ang</p>
