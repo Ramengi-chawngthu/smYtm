7170db4e6a61a98c68132121916b356c0bc0bdf9
ramengi-chawngthu <sccc@email.com>
2023-05-15 13:58:45 +0530

    Make the text responsive in Posts component

--- app/posts/[id]/SubHeading.tsx
+++ app/posts/[id]/SubHeading.tsx
@@ -7,8 +7,8 @@ function SubHeading(props: any) {
     return (
         <>
         <div className="mt-14">
-            <h2 className='text-3xl font-bold'>{subHeading}</h2>
-            <p className='text-2xl mt-4'>{body}</p>
+            <h2 className='text-2xl md:text-3xl font-bold'>{subHeading}</h2>
+            <p className='text-xl md:text-2xl mt-4'>{body}</p>
         </div>
             
         </>
