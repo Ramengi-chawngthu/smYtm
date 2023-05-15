7be518559bfe631260d9410f9ac12ec00a7e3a45
ramengi-chawngthu <sccc@email.com>
2023-05-15 14:08:54 +0530

    Fix typo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ function About() {
     </h1>
     <h1 className={"text-center text-5xl font-bold mt-20 " + styles.commission}>Thupek Ropui tak chu</h1>
     <p className="mt-8 text-2xl text-center">Tin, he Ram Chanchin Ṭha hi hnam zawng zawng hriattîrna tûrin khawvêl zawng zawngah hrilin a awm ang a; chumi zawhah chuan tâwpna chu a lo thleng ang.</p>
-    <p className="text-right text-xl mt-4 font-bold">Mattaia 24:14</p>
+    <p className="text-right text-xl mt-4 font-bold">Matthaia 24:14</p>
     </>
   )
 }
