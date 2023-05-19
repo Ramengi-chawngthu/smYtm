e88d0d9ccc4455743c2803646e0012b5f1dcff61
ramengi-chawngthu <sccc@email.com>
2023-05-19 22:06:35 +0530

    Make Emoji in about page more responsive in smaller screens

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ function About() {
         He SM & YTM Movement hi 10/40 window huam chhung rama rawngbawla kal leh la kal tur ho chanchin ziahna a ni.
       </h1>
       <div className={"px-12 py-4 mt-20" + " " + styles.localBoxShadow}>
-        <h1 className="text-center text-7xl md:text-9xl font-bold mt-24 sm:mt-28">⛪🗺⛪</h1>
+        <h1 className="text-center text-5xl md:text-9xl font-bold mt-24 sm:mt-28">⛪🗺⛪</h1>
         <h1 className={"text-center text-5xl md:text-8xl font-bold mt-14 sm:mt-32" + " " + styles.commission}>Thupek leh Thutiam Ropui tak chu  </h1>
         <h2 className="text-center text-3xl md:text-5xl mt-24 font-bold">Thupek</h2>
         <p className="mt-10 text-3xl md:text-5xl text-center">Chutichuan, kal ula, <span className={styles.commission}>hnam tina mi</span> zirtîrahte siam ula, ...</p>
